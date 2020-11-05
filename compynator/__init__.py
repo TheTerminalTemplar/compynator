eb84aed3c20768dad5e99d3b6495255f3a8979f4
Templar <theterminaltemplar@gmail.com>
2020-11-05 17:41:43 +0000

    Version 0.1.2

--- compynator/__init__.py
+++ compynator/__init__.py
@@ -16,7 +16,7 @@
 performance and support for ambiguous/non-deterministic grammars.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .core import (
         Parser, Result, One, Succeed, Success, Empty, Fail, Failure, Terminal,
